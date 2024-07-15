7aadb4cde6aede1fd7c72a654d18478970e6debc
Anton Beliaev <anton.beliaev@student.torrens.edu.au>
2024-07-15 13:55:20 +0930

    Initial commit

---
+++ src/main/java/dot503/Calculator.java
@@ -0,0 +1,19 @@
+package dot503;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Calculator {
+
+    @GetMapping("/addition/{a}/{b}")
+    public String Addition(@PathVariable Integer a, @PathVariable Integer b) {
+        return String.valueOf(a + b);
+    }
+
+    @GetMapping("/substraction/{a}/{b}")
+    public String Substraction(@PathVariable Integer a, @PathVariable Integer b) {
+        return String.valueOf(a - b);
+    }
+};
